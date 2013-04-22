84a77b8edbceff7d017244f88edca6ddfa12c1c2
Jonathan Holloway <jonathan.holloway@gmail.com>
2013-04-22 11:12:33 +0100

    Added logging info

--- buildpackagesfile.py
+++ buildpackagesfile.py
@@ -21,14 +21,14 @@ zippart = ".zip"
 extensions = targzpart, zippart
 packages_file = "PACKAGES"
 
-def get_description_name(filename):
+def get_description_name(filename):	
 	namepart=re.search("(.*)_", filename).group(1)	
 	return namepart + os.sep + "DESCRIPTION"	
 
 # Read a file from a targz archive
 def read_file_in_targz(filename):
 
-	print "Processing gzip: " + filename + " \n"
+	print "Processing gzip: " + filename
 	with tarfile.open(filename, "r:gz") as tar:		
 		descfile = get_description_name(filename)
 		content = tar.extractfile(descfile).read()		
@@ -37,7 +37,7 @@ def read_file_in_targz(filename):
 # Read a file from a zip archive
 def read_file_in_zip(filename):
 
-	print "Processing zip: " + filename + " \n"	
+	print "Processing zip: " + filename
 	with zipfile.ZipFile(filename) as zfile:
 		
 		descfile = get_description_name(filename)
@@ -71,26 +71,34 @@ def gzip_packages_file():
 
 # Build all the packages
 def build_all_packages():
-	# Backup old package file (if it exists)
+	# Backup old package file (if it exists)	
 	if os.path.isfile(packages_file):
+		print "Performing backup of old PACKAGES file"
    		shutil.move(packages_file, packages_file + ".bk")	
+   	else:
+   		print "Could not find PACKAGES"
+
+   	print "Rebuilding..."
 
 	# Look for matching files in the current directory
 	files = [ f for f in listdir(path) if isfile(join(path,f)) and is_archive(f) ]
-	for f in files:
+	print "Found " + str(len(files)) + " files to add"
+	for f in files:		
 		add_description_from_file(f)
 
 	gzip_packages_file()	
+	
 # Main method
 if  __name__ =="__main__":
 
 	parser = OptionParser()	
-	parser.add_option("-b", action="store_true", dest="buildall", default=False, help="Build all packages")
-	parser.add_option("-a", dest="packagename", help="Append a packagename to PACKAGES (e.g. ggplot2_0.9.3.1.tar.gz)")
+	parser.add_option("-b", action="store_true", dest="buildall", default=False, help="Rebuild of PACKAGES based on current directory")
+	parser.add_option("-a", dest="packagename", help="Append a packagename to existing PACKAGES file (e.g. ggplot2_0.9.3.1.tar.gz)")
 	(options, args) = parser.parse_args()
 	
 	if options.buildall:
-		build_all_packages
+		print "Building package index for all"
+		build_all_packages()
 	elif options.packagename:		
 		add_description_from_file(options.packagename)
 	else:
